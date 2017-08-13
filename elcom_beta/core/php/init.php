1a3584181ff1214d4173494f75455cadda0784c5
DenislavBelchev92 <denislav.belchev92@gmail.com>
2017-08-13 11:18:37 +0300

    Remove_old_EC

--- elcom_beta/core/php/init.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-	
-	require $_SERVER['DOCUMENT_ROOT']."/elcom_beta/core/php/helpers/database.php";
-	require $_SERVER['DOCUMENT_ROOT']."/elcom_beta/core/php/helpers/users.php";
-	require $_SERVER['DOCUMENT_ROOT']."/elcom_beta/core/php/helpers/general.php";
-	
-	$errors = array();
-?>
